6abeff655fac5797b02d3755df7751e0973f6178
Jonathan Frame <jmframe@crimson.ua.edu>
2020-09-13 11:23:15 -0400

    objective functino for fluxes

--- noah-mp/src/rmse_obj_fun.f90
+++ noah-mp/src/rmse_obj_fun.f90
@@ -7,33 +7,35 @@ subroutine rmse_obj_fun(mdate,mtime,state,output,Nt)!ofval,errcode)
  ! variable declarations
  ! dimensions
  integer, intent(in) :: Nt
- integer, parameter :: Dx = 4
+ integer, parameter :: Dx = 3
  integer :: ti, xi
- integer :: Gx
+ integer, dimension(Dx) :: Gx
 
  ! dates
  integer, dimension(Nt,3) :: mdate, odate
- real, dimension(Nt) :: mtime, otime
+ real, dimension(Nt,2) :: mtime, otime
  real :: dummy
 
  ! obs/mod vars
- real, dimension(Nt) :: modl, obsv
+ real, dimension(Nt,Dx) :: modl, obsv
 
  ! model vars
  type(state_data),   intent(in), dimension(Nt) :: state
  type(output_data),  intent(in), dimension(Nt) :: output
 
  ! objective function variables
- real    :: sse, nsmean, nsmeandiff
+ real, dimension(Dx) :: sse, nsmean, nsmeandiff
 
  ! I/O parms
  integer, parameter :: fid = 136
 
  ! ------------------------------------------------------------------
  ! read observation file
+ ! jmframe: PLUMBER-2 flux data Ordered from text file make from NetCDF data.
+ ! year,month,day,hour,minute,NEE,GPP,Qle,Qh
  open(fid,file='obs.txt')
   do ti = 1,Nt
-   read(fid,*) odate(ti,:),otime(ti),obsv(ti),dummy
+   read(fid,*) odate(ti,:),otime(ti,:),obsv(ti,1),dummy,obsv(ti,2:3)
   enddo ! times
  close(fid)
 
@@ -42,7 +44,9 @@ subroutine rmse_obj_fun(mdate,mtime,state,output,Nt)!ofval,errcode)
  do ti = 1,Nt
   if ((odate(ti,1).ne.mdate(ti,1)).or.  &
       (odate(ti,2).ne.mdate(ti,2)).or.  &
-      (otime(ti)  .ne.mtime(ti))) then
+      (odate(ti,3).ne.mdate(ti,3)).or.  &
+      (otime(ti,1).ne.mtime(ti,1)).or.  &
+      (otime(ti,2).ne.mtime(ti,2))) then
     print*, 'mismatch_obs_dates'
     stop 111
    return
@@ -52,60 +56,56 @@ subroutine rmse_obj_fun(mdate,mtime,state,output,Nt)!ofval,errcode)
  ! ------------------------------------------------------------------
  ! extract pertinent modeled dimensions
  do ti = 1,Nt
-  modl(ti) = state(ti)%smc(1)
+  modl(ti,1) = output(ti)%nee
+  modl(ti,2) = output(ti)%qe 
+  modl(ti,3) = output(ti)%qh
  enddo ! times 
 
  ! ------------------------------------------------------------------
  ! calcualte squared errors for each output dimension
+ ! And calc the mean for NSE
  sse = 0.
  Gx = 0
  nsmean = 0. 
-do ti = 1,Nt
-  if ((obsv(ti).gt.-9990).and.(modl(ti).gt.-9990)) then
-   sse = sse + ((obsv(ti) - modl(ti))**2)
-   Gx = Gx + 1
-   ! Calculate the record mean for a Nash Sutcliffe efficiency
-   nsmean = nsmean + obsv(ti)
-   ! calcualte anomaly (diff from mean) for each output dimension
-   ! for the Nash-Sutcliffe efficiency
-  endif 
+ do ti = 1,Nt
+   do xi = 1,Dx
+     if ((obsv(ti,xi).gt.-9990).and.(modl(ti,xi).gt.-9990)) then
+       sse(xi) = sse(xi) + ((obsv(ti,xi) - modl(ti,xi))**2)
+       Gx(xi) = Gx(xi) + 1
+       ! Calculate the record mean for a Nash Sutcliffe efficiency
+       ! will devide by number of good records in next loop
+       nsmean(xi) = nsmean(xi) + obsv(ti,xi) 
+     endif
+   enddo 
  enddo
-
- ! ------------------------------------------------------------------
+ ! Divide by good records to get mean
+ do xi = 1,Dx
+   nsmean(xi) = nsmean(xi)/Gx(xi)
+ enddo
  ! calcualte anomaly (diff from mean) for each output dimension
  ! for the Nash-Sutcliffe efficiency
- nsmean = nsmean/Gx
  nsmeandiff = 0.
  do ti = 1,Nt
-  if ((obsv(ti).gt.-9990).and.(modl(ti).gt.-9990)) then
-   nsmeandiff = nsmeandiff + ((obsv(ti) - nsmean)**2)
-  endif 
+   do xi = 1,Dx
+     if ((obsv(ti,xi).gt.-9990).and.(modl(ti,xi).gt.-9990)) then
+       nsmeandiff(xi) = nsmeandiff(xi) + ((obsv(ti,xi) - nsmean(xi))**2)
+     endif
+   enddo 
  enddo
-
- ! ------------------------------------------------------------------
- ! write to output file - Mean Sum of Squared Error
- !open(fid,file='noahmp_objfun.out')
- !do xi = 1,Dx
- ! write(fid,*) sse(xi)/Gx(xi),','
- !enddo
- !close(fid)
-
  ! ------------------------------------------------------------------
  ! write to output file - Root Mean Sum of Squared Error
  open(fid,file='noahmp_objfun.out')
- write(fid,*) sqrt(sse/Gx),','
-  print*, 'RMSE = ', sqrt(sse/Gx)
+ write(fid,*) "Root Mean Sum of Squared Error"
+ do xi=1,Dx
+   write(fid,*) sqrt(sse(xi)/Gx(xi)),','
+   print*, 'RMSE = ', sqrt(sse(xi)/Gx(xi))
+ enddo
+ write(fid,*) "Nash-Sutcliffe efficiency"
+ do xi = 1,Dx
+   write(fid,*) (sse(xi)/nsmeandiff(xi)),','
+   print*, 'NSE = ', 1-(sse(xi)/nsmeandiff(xi))
+ enddo
  close(fid)
-
- ! ------------------------------------------------------------------
- ! write to output file - AS NASH-SUTCLIFFE EFFICIENCY
- ! NOTE: can't do 1-(sse/nsmeandiff) because ostrich only minimizes the obj.
- ! So I just removed the 1-
- !open(fid,file='noahmp_objfun.out')
- !do xi = 1,Dx
- ! write(fid,*) (sse(xi)/nsmeandiff(xi)),','
- !enddo
- !close(fid)
 
  ! ------------------------------------------------------------------
  return
